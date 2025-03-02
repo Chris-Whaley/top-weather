bdbc100a4ee8214b5b522e119dd88ed36b0551e7
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2025-03-01 20:57:41 -0500

    init: Lookup created to match icon filenames and api response weather variable for icons

---
+++ src/weather-icons.js
@@ -0,0 +1,13 @@
+const weatherIcons = {
+  // Key = icon value from api call
+  // Value = image filename
+  snow: "Heavy-Snow.gif",
+  rain: "Rain.gif",
+  fog: "Fog.gif",
+  wind: "Windy.gif",
+  cloudy: "Cloudy.gif",
+  "partly-cloudy-day": "Partly-Cloudy.gif",
+  "partly-cloudy-night": "Partly-Clear.gif",
+  "clear-day": "Sunny.gif",
+  "clear-night": "Clear.gif",
+};
